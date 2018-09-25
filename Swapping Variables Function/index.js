1e4896731cd100f32877918ef5a6ec1340e0b96e
MahmoudSaleh15 <43014605+MahmoudSaleh15@users.noreply.github.com>
2018-09-25 13:53:54 -0600

    update

---
+++ Swapping Variables Function/index.js
@@ -0,0 +1,15 @@
+// Create 10 variables about yourself
+let MahmoudSaleh = [];
+let MeganSpence = [];
+
+// firstName, LastName, age, ageAfraid, hair, male, female
+MahmoudSaleh = ["Mahmoud", "Saleh", 13, 29, null, true, false];
+MeganSpence = ["Megann", "Spence", 14, null, true, false, true];
+
+// Printing out entire object
+console.log(MahmoudSaleh);
+console.log(MeganSpence);
+
+// Print variables to console
+console.log("Last Name of MahmoudSaleh is: " + MahmoudSaleh[1]);
+console.log("Last Name MeganSpence is: " + MeganSpence[1]);
---
+++ Swapping Variables/index.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+// Create 10 variables about yourself
+let name = "Mahmoud";
+let lastname = "Saleh";
+
+// new code goes here
+// Add a third varible
+let memory = name;
+name = lastname;
+lastname = memory;
+
+// Print variables to console
+console.log("My first name is: " + name);
+console.log("My first lastname is: " + lastname);
