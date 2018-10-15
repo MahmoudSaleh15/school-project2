52e89e99d7f83044b446fc098f6919eb013b5b3f
MahmoudSaleh15 <43014605+MahmoudSaleh15@users.noreply.github.com>
2018-10-15 14:02:48 -0600

    hh

--- Mahmoud's Websites/JavaScript Lessons/FizzBuzz/index.js
+++ Mahmoud's Websites/JavaScript Lessons/FizzBuzz/index.js
@@ -1,7 +1,30 @@
-//Reminder: test all the test cases
-const testNumber = 7; //issue of safety
-console.log(fizzBuzz(testNumber));
+(function (window) {
+    'use strict';
+    var counter = 1;
+    var limit = 100;
+    var options = {
+      3: 'Fizz',
+      5: 'Buzz',
+      'default': function (input) {
+        return input;
+      }
+    };
 
-function fizzBuzz (input) {
-  //Logic of Problem needs to be copied and organized
-}
+    var noop = function () {
+      return;
+    };
+
+    var print = function (input) {
+      var output = [];
+      Object.keys(options).map(
+        function (key) {
+          (((typeof options[key] !== 'function') && ((parseInt(input, 10) % parseInt(key, 10)) === 0)) ? Array.prototype.push : noop).call(output, options[key]);
+        }
+      );
+      return console.log.call(console, output.length ? output.join('') : options.default.call(this, input));
+    };
+
+    for (; counter <= limit; counter += 1) {
+      print.call(this, counter);
+    }
+})(this);
--- Mahmoud's Websites/JavaScript Lessons/LandScape or Portrait/index.js
+++ Mahmoud's Websites/JavaScript Lessons/LandScape or Portrait/index.js
@@ -1,8 +1,8 @@
-function isLandscape (___, ___) {
-  if (___ == ___) return 'Square'; //must be first, return will not allow rest of code to run
-  return (___ > ___) ? 'Landscape' : 'Portrait'; //returns the value of the expression
+function isLa (width, height) {
+  if (width == height) return 'Square'; //must be first, return will not allow rest of code to run
+  return (width > height) ? 'Landscape' : 'Portrait'; //ternary operator example
 }
 
-let width = 800;
-let height = 800;
+let width = 800; //the witdth of the Landscape or Portrait
+let height = 600; //the height of the Landscape or Portrait
 console.log("This display geometry is", isLandscape (width, height)); //Difference between calling functions with arguements and sending to parameter's, local variables
